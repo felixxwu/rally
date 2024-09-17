0b3a08e3184b86cfa61500aff49a1f1834800f2a
Felix Wu <fwu@shipserv.com>
2024-09-17 21:49:35 +0100

    debug true by default

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import React from "react";
-import { KeyboardControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { Physics } from "@react-three/rapier";
-import { Suspense, useMemo } from "react";
-import { Experience } from "./components/Experience";
+import React from 'react'
+import { KeyboardControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Physics } from '@react-three/rapier'
+import { Suspense, useMemo } from 'react'
+import { Experience } from './components/Experience'
 
 export const Controls = {
-  forward: "forward",
-  back: "back",
-  left: "left",
-  right: "right",
-  jump: "jump",
-};
+  forward: 'forward',
+  back: 'back',
+  left: 'left',
+  right: 'right',
+  jump: 'jump',
+}
 
 function App() {
-  const [speed, setSpeed] = React.useState(0);
-  const [debug, setDebug] = React.useState(false);
-  
+  const [speed, setSpeed] = React.useState(0)
+  const [debug, setDebug] = React.useState(true)
+
   const map = useMemo(
     () => [
-      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
-      { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
-      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
-      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-      { name: Controls.jump, keys: ["Space"] },
+      { name: Controls.forward, keys: ['ArrowUp', 'KeyW'] },
+      { name: Controls.back, keys: ['ArrowDown', 'KeyS'] },
+      { name: Controls.left, keys: ['ArrowLeft', 'KeyA'] },
+      { name: Controls.right, keys: ['ArrowRight', 'KeyD'] },
+      { name: Controls.jump, keys: ['Space'] },
     ],
     []
-  );
+  )
 
   return (
     <>
       <KeyboardControls map={map}>
         <Canvas shadows camera={{ fov: 60 }}>
-          <color attach="background" args={["#ececec"]} />
+          <color attach='background' args={['#ececec']} />
           <Suspense>
             <Physics>
               <Experience setSpeed={setSpeed} debug={debug} />
@@ -40,10 +40,12 @@ function App() {
           </Suspense>
         </Canvas>
       </KeyboardControls>
-      <h1 style={{color: 'black', position: 'fixed', bottom: 0}}>{Math.round(speed * 100)}</h1>
-      <h1 style={{color: 'black', position: 'fixed', top: 0}} onClick={() => setDebug(!debug)}>Debug: {`${debug}`}</h1>
+      <h1 style={{ color: 'black', position: 'fixed', bottom: 0 }}>{Math.round(speed * 100)}</h1>
+      <h1 style={{ color: 'black', position: 'fixed', top: 0 }} onClick={() => setDebug(!debug)}>
+        Debug: {`${debug}`}
+      </h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,48 +1,57 @@
-import React from "react";
-import { Box, useKeyboardControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { useRef } from "react";
-import { Controls } from "../App";
-import * as THREE from "three";
+import React from 'react'
+import { Box, useKeyboardControls } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { RapierRigidBody, RigidBody } from '@react-three/rapier'
+import { useRef } from 'react'
+import { Controls } from '../App'
+import * as THREE from 'three'
 
-export const Experience = ({setSpeed, debug}: {setSpeed: (speed: number) => void, debug: boolean}) => {
-  const cube = useRef<RapierRigidBody | null>(null);
-  const directionOfTravelArrow = useRef<THREE.ArrowHelper | null>(null);
-  const sideForceArrow = useRef<THREE.ArrowHelper | null>(null);
-  const airResistanceArrow = useRef<THREE.ArrowHelper | null>(null);
-  const carForceArrow = useRef<THREE.ArrowHelper | null>(null);
-  const oldPos = useRef(new THREE.Vector3(0, 0, 0));
-  const isOnFloor = useRef(true);
+export const Experience = ({
+  setSpeed,
+  debug,
+}: {
+  setSpeed: (speed: number) => void
+  debug: boolean
+}) => {
+  const cube = useRef<RapierRigidBody | null>(null)
+  const directionOfTravelArrow = useRef<THREE.ArrowHelper | null>(null)
+  const sideForceArrow = useRef<THREE.ArrowHelper | null>(null)
+  const airResistanceArrow = useRef<THREE.ArrowHelper | null>(null)
+  const carForceArrow = useRef<THREE.ArrowHelper | null>(null)
+  const oldPos = useRef(new THREE.Vector3(0, 0, 0))
+  const isOnFloor = useRef(true)
 
-  const spacePressed = useKeyboardControls((state) => state[Controls.jump]);
-  const leftPressed = useKeyboardControls((state) => state[Controls.left]);
-  const rightPressed = useKeyboardControls((state) => state[Controls.right]);
-  const backPressed = useKeyboardControls((state) => state[Controls.back]);
-  const forwardPressed = useKeyboardControls(
-    (state) => state[Controls.forward]
-  );
+  const spacePressed = useKeyboardControls(state => state[Controls.jump])
+  const leftPressed = useKeyboardControls(state => state[Controls.left])
+  const rightPressed = useKeyboardControls(state => state[Controls.right])
+  const backPressed = useKeyboardControls(state => state[Controls.back])
+  const forwardPressed = useKeyboardControls(state => state[Controls.forward])
 
   const power = 0.15
   const maxTireForce = 0.3
   const steer = 0.06
   const airResistanceScale = 0.6
 
-  const handleMovement = (tireForce: THREE.Vector3, airResistance: THREE.Vector3, angle: number, speed: number) => {
+  const handleMovement = (
+    tireForce: THREE.Vector3,
+    airResistance: THREE.Vector3,
+    angle: number,
+    speed: number
+  ) => {
     if (!isOnFloor.current || !cube.current) {
-      return;
+      return
     }
-    const steerTorque = speed < 0.01 ? 0 : angle > 1 ? -steer : steer;
+    const steerTorque = speed < 0.01 ? 0 : angle > 0.8 ? -steer : steer
     if (rightPressed) {
-      cube.current.applyTorqueImpulse({ x: 0, y: steerTorque, z: 0 }, true);
+      cube.current.applyTorqueImpulse({ x: 0, y: steerTorque, z: 0 }, true)
     }
     if (leftPressed) {
-      cube.current.applyTorqueImpulse({ x: 0, y: -steerTorque, z: 0 }, true);
+      cube.current.applyTorqueImpulse({ x: 0, y: -steerTorque, z: 0 }, true)
     }
 
     const carForce = new THREE.Vector3(0, 0, 0)
     carForce.add(tireForce)
-    
+
     if (forwardPressed && !spacePressed) {
       const engineForce = new THREE.Vector3(0, 0, -power)
       engineForce.applyQuaternion(cube.current.rotation())
@@ -57,33 +66,32 @@ export const Experience = ({setSpeed, debug}: {setSpeed: (speed: number) => void
     }
 
     carForce.clampLength(0, maxTireForce)
-    
-    const {x ,y ,z} = cube.current.translation();
-    carForceArrow.current?.position.set(x, y + 1, z);
-    carForceArrow.current?.setDirection(carForce);
-    carForceArrow.current?.setLength(carForce.length() * 10);
-    carForceArrow.current?.setColor(new THREE.Color(0xff0000));
+
+    const { x, y, z } = cube.current.translation()
+    carForceArrow.current?.position.set(x, y + 1, z)
+    carForceArrow.current?.setDirection(carForce)
+    carForceArrow.current?.setLength(carForce.length() * 10)
+    carForceArrow.current?.setColor(new THREE.Color(0xff0000))
 
     carForce.add(airResistance)
 
-    cube.current.applyImpulse(carForce, true);
-  };
-
+    cube.current.applyImpulse(carForce, true)
+  }
 
   useFrame((_state, delta) => {
     if (!cube.current) return
-    
-    const {x ,y ,z} = cube.current.translation();
+
+    const { x, y, z } = cube.current.translation()
     const carPos = new THREE.Vector3(x, y, z)
-    const rotation = cube.current.rotation();
-    
+    const rotation = cube.current.rotation()
+
     const forwardVec = new THREE.Vector3(0, 0, 10)
     forwardVec.applyQuaternion(rotation)
     const cameraPos = carPos.clone().add(forwardVec.clone()).add(new THREE.Vector3(0, 5, 0))
     // _state.camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
-    _state.camera.position.lerp(cameraPos, delta * 10);
-    _state.camera.lookAt(carPos.clone());
-    _state.camera.updateProjectionMatrix();
+    _state.camera.position.lerp(cameraPos, delta * 10)
+    _state.camera.lookAt(carPos.clone())
+    _state.camera.updateProjectionMatrix()
 
     const directionOfTravel = carPos.clone().sub(oldPos.current)
     const speed = directionOfTravel.length()
@@ -98,30 +106,33 @@ export const Experience = ({setSpeed, debug}: {setSpeed: (speed: number) => void
 
     const projected = directionOfTravel.clone().projectOnVector(sideForce)
     const sideTireForce = projected.multiplyScalar(-10).clampLength(0, maxTireForce)
-    const tireForce = spacePressed ? directionOfTravel.clone().multiplyScalar(-10).clampLength(0, maxTireForce) : sideTireForce
+    const tireForce = spacePressed
+      ? directionOfTravel.clone().multiplyScalar(-10).clampLength(0, maxTireForce)
+      : sideTireForce
 
-    const airResistance = directionOfTravel.clone().multiplyScalar(directionOfTravel.length() * -airResistanceScale)
+    const airResistance = directionOfTravel
+      .clone()
+      .multiplyScalar(directionOfTravel.length() * -airResistanceScale)
 
-    handleMovement(tireForce, airResistance, angle, speed);
+    handleMovement(tireForce, airResistance, angle, speed)
 
-    directionOfTravelArrow.current?.position.set(x, y + 1, z);
-    directionOfTravelArrow.current?.setDirection(directionOfTravel);
-    directionOfTravelArrow.current?.setLength(speed * 10);
-    directionOfTravelArrow.current?.setColor(new THREE.Color(0x0000ff));
+    directionOfTravelArrow.current?.position.set(x, y + 1, z)
+    directionOfTravelArrow.current?.setDirection(directionOfTravel)
+    directionOfTravelArrow.current?.setLength(speed * 10)
+    directionOfTravelArrow.current?.setColor(new THREE.Color(0x0000ff))
 
-    sideForceArrow.current?.position.set(x, y + 1, z);
-    sideForceArrow.current?.setDirection(tireForce);
-    sideForceArrow.current?.setLength(tireForce.length() * 10);
-    sideForceArrow.current?.setColor(new THREE.Color(0x000000));
+    sideForceArrow.current?.position.set(x, y + 1, z)
+    sideForceArrow.current?.setDirection(tireForce)
+    sideForceArrow.current?.setLength(tireForce.length() * 10)
+    sideForceArrow.current?.setColor(new THREE.Color(0x000000))
 
-    airResistanceArrow.current?.position.set(x, y + 1, z);
-    airResistanceArrow.current?.setDirection(airResistance.clone());
-    airResistanceArrow.current?.setLength(airResistance.length() * 10);
-    airResistanceArrow.current?.setColor(new THREE.Color(0xffffff));
+    airResistanceArrow.current?.position.set(x, y + 1, z)
+    airResistanceArrow.current?.setDirection(airResistance.clone())
+    airResistanceArrow.current?.setLength(airResistance.length() * 10)
+    airResistanceArrow.current?.setColor(new THREE.Color(0xffffff))
 
-    oldPos.current.set(x, y, z);
-  });
-
+    oldPos.current.set(x, y, z)
+  })
 
   return (
     <>
@@ -133,18 +144,18 @@ export const Experience = ({setSpeed, debug}: {setSpeed: (speed: number) => void
         ref={cube}
         angularDamping={3}
         onCollisionEnter={({ other }) => {
-          if (other.rigidBodyObject?.name === "floor") {
-            isOnFloor.current = true;
+          if (other.rigidBodyObject?.name === 'floor') {
+            isOnFloor.current = true
           }
         }}
         onCollisionExit={({ other }) => {
-          if (other.rigidBodyObject?.name === "floor") {
-            isOnFloor.current = false;
+          if (other.rigidBodyObject?.name === 'floor') {
+            isOnFloor.current = false
           }
         }}
       >
         <Box args={[1, 0.6, 2]}>
-          <meshStandardMaterial color="royalblue" />
+          <meshStandardMaterial color='royalblue' />
         </Box>
       </RigidBody>
 
@@ -157,23 +168,21 @@ export const Experience = ({setSpeed, debug}: {setSpeed: (speed: number) => void
         </>
       )}
 
-      {
-        [...Array(60)].map((_, z) => (
-          <RigidBody type="kinematicPosition" position={[0, 0.75, 0]} key={z} >
-            <group position={[2.5, 0, z * 10 - 300]}>
-              <Box args={[5, 1, 0.5]}>
-                <meshStandardMaterial color="peachpuff" />
-              </Box>
-            </group>
-          </RigidBody>
-        ))
-      }
+      {[...Array(60)].map((_, z) => (
+        <RigidBody type='kinematicPosition' position={[0, 0.75, 0]} key={z}>
+          <group position={[2.5, 0, z * 10 - 300]}>
+            <Box args={[5, 1, 0.5]}>
+              <meshStandardMaterial color='peachpuff' />
+            </Box>
+          </group>
+        </RigidBody>
+      ))}
 
-      <RigidBody type="fixed" name="floor" friction={0}>
+      <RigidBody type='fixed' name='floor' friction={0}>
         <Box position={[0, 0, 0]} args={[50, 1, 600]}>
-          <meshStandardMaterial color="springgreen" />
+          <meshStandardMaterial color='springgreen' />
         </Box>
       </RigidBody>
     </>
-  );
-};
+  )
+}
