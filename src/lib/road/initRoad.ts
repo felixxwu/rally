fa2c16a716ee2e37da117c5d1704fe24966b504b
Felix Wu <fwu@shipserv.com>
2024-09-23 13:44:02 +0100

    refactor noise func

--- src/lib/road/initRoad.ts
+++ src/lib/road/initRoad.ts
@@ -1,9 +1,19 @@
 import AmmoType from 'ammojs-typed';
 declare const Ammo: typeof AmmoType;
 
-import { physicsWorld, roadMesh, scene } from '../../refs';
+import {
+  physicsWorld,
+  roadMesh,
+  scene,
+  terrainDepth,
+  terrainMaxHeight,
+  terrainMinHeight,
+  terrainWidth,
+  terrainWidthExtents,
+} from '../../refs';
 import { createRoadShape, Triangle, Vector } from './createRoadShape';
 import { createRoadTriangles } from './createRoadTriangles';
+import { createNoiseFunc } from '../terrain/generateHeight';
 
 export function initRoad() {
   const vecs = [...Array(10000)].map(
@@ -11,10 +21,28 @@ export function initRoad() {
       [Math.sin(i * 0.05) * 40 - 20, Math.sin(i * 0.02) * 20 - 3 + i * 0.1, i * 2 + 50] as Vector
   );
 
+  // const vecs = [] as Vector[];
+  // const point = { x: 0, z: 0 };
+  // const noise = createNoiseFunc(3, 8);
+  // const hRange = terrainMaxHeight - terrainMinHeight;
+
+  // for (let i = 0; i < terrainWidth; i += 0.02) {
+  //   const [x, z] = toWorldPos(i, Math.sin(i / 4) * (terrainWidth / 2) + terrainWidth / 2);
+  //   const height =
+  //     Math.pow(noise(i, Math.sin(i / 4) * (terrainWidth / 2) + terrainWidth / 2), 2) * hRange +
+  //     terrainMinHeight;
+  //   vecs.push([x, height + 5, z] as Vector);
+  // }
+
   const triangles = createRoadTriangles(vecs);
 
   const { rigidBody, mesh } = createRoadShape(triangles);
   scene.current?.add(mesh);
   roadMesh.current = mesh;
   physicsWorld.current?.addRigidBody(rigidBody);
+}
+
+function toWorldPos(x: number, z: number) {
+  const mult = terrainWidthExtents / terrainWidth;
+  return [(terrainWidth / 2 - x - 1) * -mult, (terrainWidth / 2 - z) * -mult];
 }
--- src/lib/terrain/generateHeight.ts
+++ src/lib/terrain/generateHeight.ts
@@ -2,21 +2,25 @@ import { createNoise2D } from 'simplex-noise';
 import { scale, terrainDepth, terrainMaxHeight, terrainMinHeight, terrainWidth } from '../../refs';
 import { ref } from '../utils/ref';
 
-let seedRef = ref(1);
-function random() {
-  var x = Math.sin(seedRef.current++) * 10000;
-  return x - Math.floor(x);
-}
+let seedRef = ref(0);
 
-export function generateHeight(seed: number) {
+export function createNoiseFunc(seed: number, level: number) {
   seedRef.current = seed;
-
-  const simplex = createNoise2D(random);
+  const simplex = createNoise2D(() => {
+    var x = Math.sin(seedRef.current++) * 10000;
+    return x - Math.floor(x);
+  });
 
   const noise = (level, x, z) =>
     simplex(scale + level * x * scale, scale + level * z * scale) / level +
     (level > 1 ? noise(level / 2, x, z) : 0) +
     0.1;
+
+  return (x: number, y: number) => noise(level, x, y);
+}
+
+export function generateHeight(seed: number, level: number) {
+  const noise = createNoiseFunc(seed, level);
 
   const size = terrainWidth * terrainDepth;
   const data = new Float32Array(size);
@@ -27,7 +31,7 @@ export function generateHeight(seed: number) {
 
   for (let j = 0; j < terrainDepth; j++) {
     for (let i = 0; i < terrainWidth; i++) {
-      const height = Math.pow(noise(8, i, j), 2) * hRange + terrainMinHeight;
+      const height = Math.pow(noise(i, j), 2) * hRange + terrainMinHeight;
 
       data[p] = height;
 
