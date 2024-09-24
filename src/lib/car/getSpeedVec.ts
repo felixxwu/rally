1fef15bf7cbcf099b98b0588527492115675c0be
Felix Wu <fwu@shipserv.com>
2024-09-25 00:53:35 +0100

    delta time fix?

--- src/lib/car/getSpeedVec.ts
+++ src/lib/car/getSpeedVec.ts
@@ -2,5 +2,5 @@ import { getOldNewPosDiff } from './getDirectionOfTravel';
 
 export function getSpeedVec(deltaTime: number) {
   const diff = getOldNewPosDiff();
-  return diff.clone().setLength(diff.length() / deltaTime);
+  return diff.clone().divideScalar(deltaTime);
 }
